9e6af9735518e8a0e6c8294e670e6736eea69eff
mwanjajoel <joelsilverworks@gmail.com>
2019-12-29 12:04:55 +0300

    removed automatic playback by the OS

--- text2speech.py
+++ text2speech.py
@@ -10,6 +10,5 @@ tts = gTTS(text=sample_text, lang="en-uk")
 # save the mp3 to the current directory
 tts.save("sample_voice.mp3")
 
-# play the audio using the default audio player on your system
-os.system("start sample_voice.mp3")
+
 
